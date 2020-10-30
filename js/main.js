d66b4feda6e53cfb5bf84adc169067c00eb87e05
bangWonbae <73636453+bangWonbae@users.noreply.github.com>
2020-10-30 17:32:58 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $('.con04Menu').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      prevArrow:'<div class="sliderArrow arrowL"><img src="images/con04arrowLeft.png"></div>',
+      nextArrow:'<div class="sliderArrow arrowR"><img src="images/con04arrowRight.png"></div>'
+    });
+})
